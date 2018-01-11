9b15dd62990def9a8aea513ff7a94a5807fe6dfe
Christopher Munden <cmunden@unca.edu>
2018-01-11 18:48:42 -0500

    Added Abstract classes

---
+++ Assets/scripts/entity.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class entity : ExposableMonobehaviour
+{
+    [HideInInspector, SerializeField]
+    private string name;
+    [HideInInspector, SerializeField]
+    private bool interactable;
+
+    public entity(string name, bool interactable)
+    {
+        this.name = name;
+        this.interactable = interactable;
+    }
+
+    [ExposeProperty]
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    [ExposeProperty]
+    public bool Interactable
+    {
+        get { return interactable; }
+        set { interactable = value; }
+    }
+}
---
+++ Assets/scripts/fighter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fighter : entity {
+    [HideInInspector, SerializeField]
+    private int currenthp;
+    [HideInInspector, SerializeField]
+    private int maxhp;
+    [HideInInspector, SerializeField]
+    private bool hostile;
+
+    public fighter(string name, bool interactable, int currenthp, int maxhp, bool hostile) : base(name, interactable)
+    {
+        this.currenthp = currenthp;
+        this.maxhp = maxhp;
+        this.hostile = hostile;
+    }
+
+    [ExposeProperty] public int Currenthp
+    {
+        get { return currenthp; }
+        set { currenthp = value; }
+    }
+
+    [ExposeProperty]
+    public int Maxhp
+    {
+        get { return maxhp; }
+        set { maxhp = value; }
+    }
+
+    [ExposeProperty]
+    public bool Hostile
+    {
+        get { return hostile; }
+        set { hostile = value; }
+    }
+
+}
