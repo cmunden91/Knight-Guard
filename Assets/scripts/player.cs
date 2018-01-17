4e3b55255ffc1574f2eef77977f737582fae6145
Christopher Munden <cmunden@unca.edu>
2018-01-17 16:20:08 -0500

    Modified Input. Corrected player Constructed and added testing textures for player/surfaces.

---
+++ Assets/scripts/player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : fighter {
+    private static string name = "Player";
+    private static bool interactable = false;
+    private static int defaulthealth = 100;
+    private static bool hostile = false;
+    public player() : base(name, interactable, defaulthealth, defaulthealth, hostile)
+    {
+
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    
+}
