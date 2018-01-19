a05a1e47c79dd54ae06a9d565ed286599b8d9298
Christopher Munden <cmunden@unca.edu>
2018-01-19 17:09:56 -0500

    Added jump and Horizontal Movement. Horizontal movement will need a few modifications.

--- Assets/scripts/playercontroller.cs
+++ Assets/scripts/playercontroller.cs
@@ -3,18 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class playercontroller : MonoBehaviour {
-  
+    Rigidbody2D rb;
+    player play;
 
 	// Use this for initialization
 	void Start () {
-		
+        rb = GetComponent<Rigidbody2D>();
+        play = GetComponent<player>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetButtonDown("Jump"))
         {
-            GetComponent<Rigidbody2D>().velocity = Vector2.up * 6;
+            int jump = play.Jumpheight;
+            rb.velocity = Vector2.up * jump;
         }
-	}
+        if (Input.GetAxis ("Horizontal") > 0)
+        {
+            int move = play.Movementspeed;
+            rb.AddForce(Vector2.right, ForceMode2D.Impulse);
+        }
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            int move = play.Movementspeed;
+            rb.AddForce(Vector2.left, ForceMode2D.Impulse);
+        }
+    }
 }
