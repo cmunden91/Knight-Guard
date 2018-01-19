a12edeb2a91ab3d9bf608d3f94e176ba4b38cddb
Christopher Munden <cmunden@unca.edu>
2018-01-19 16:45:47 -0500

    Restored Input Manager. Added basic jump.

---
+++ Assets/scripts/playercontroller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontroller : MonoBehaviour {
+  
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Jump"))
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.up * 6;
+        }
+	}
+}
