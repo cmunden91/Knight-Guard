bc9a403ca40a8a89b118802fff7bf5a122c7ab1a
Christopher Munden <cmunden@unca.edu>
2018-02-10 16:11:46 -0500

    Fixed issue of player sticking to walls. Also begun work on wallclimbing.

--- Assets/scripts/playercontroller.cs
+++ Assets/scripts/playercontroller.cs
@@ -21,34 +21,37 @@ public class playercontroller : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-		rb.freezeRotation = true;
     }
 
     // Update is called once per frame
     void FixedUpdate()
-    {
-        isgrounded = Physics2D.OverlapCircle(ground.position, groundcircleradius, whatisground);
-        if (Input.GetButtonDown("Jump"))
-        {
-            if (isgrounded == true)
-            {
-                int jump = play.Jumpheight;
-                rb.velocity = Vector2.up * jump;
-            }
-        }
-        if (Input.GetAxis("Horizontal") > 0)
-        {
-            int move = play.Movementspeed;
-            rb.velocity = new Vector2(play.Movementspeed, rb.velocity.y);
-        }
-        if (Input.GetAxis("Horizontal") < 0)
-        {
-            int move = play.Movementspeed;
-            rb.velocity = new Vector2(0 - play.Movementspeed, rb.velocity.y);
-        }
+	{
+		isgrounded = Physics2D.OverlapCircle (ground.position, groundcircleradius, whatisground);
+		if (Input.GetButtonDown ("Jump")) {
+			if (isgrounded == true) {
+				int jump = play.Jumpheight;
+				rb.velocity = Vector2.up * jump;
+			}
+		}
+		if (Input.GetAxis ("Horizontal") > 0) {
+			int move = play.Movementspeed;
+			rb.velocity = new Vector2 (play.Movementspeed, rb.velocity.y);
+		}
+		if (Input.GetAxis ("Horizontal") < 0) {
+			int move = play.Movementspeed;
+			rb.velocity = new Vector2 (0 - play.Movementspeed, rb.velocity.y);
+		}
         
+	}
+	void OnCollisionEnter(Collision collision) {
+		Debug.Log(collision);
+	}
+    [HideInInspector]
+    public bool Isgrounded
+    {
+        get { return isgrounded; }
+        set { isgrounded = value; }
     }
-
 
 
 }
---
+++ Assets/scripts/wallgrab.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wallgrab : MonoBehaviour {
+	[SerializeField]
+	private BoxCollider2D collider;
+    [SerializeField]
+    private PhysicsMaterial2D wallclimb;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter2D(Collider2D col) {
+        if (col.GetComponent<player>() != null)
+        {
+            col.GetComponent<playercontroller>().Isgrounded = true;
+            collider.sharedMaterial = wallclimb; 
+        }
+	}
+
+}
