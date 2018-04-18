b6e99f592f8babee84cf2039c7731c625dca4729
Christopher Munden <cmunden@unca.edu>
2018-04-18 07:30:47 -0400

    Started working on health pickups

---
+++ Assets/scripts/Healthpotion.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Healthpotion : MonoBehaviour {
+    [SerializeField]
+    int healamount;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.transform.GetComponent<player>().Currenthp+=healamount;
+            Destroy(gameObject);
+    }
+}
